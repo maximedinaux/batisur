624610feef147a27499db91ede897a3353de9e91
« maximedinaux » <maxime@dinaux.be>
2020-05-12 01:09:26 +0200

    facebook card

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -23,6 +23,14 @@ const SEO = ({ title, dsc, image }) => {
     <Helmet htmlAttributes={{ lang: "fr" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={dsc || siteDsc} />
       <meta name="image" content={image} />
+      {/* facebook card */}
+      <meta property="og:url" content={siteUrl} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={dsc || siteDsc} />
+      <meta property="og:image" content={`${siteUrl}/${siteImage}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
       {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={siteTitle} />
